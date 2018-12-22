37b3f0db2ef072d3b3539e6bf470fe0db47071ab
Rich Edmunds <xBLADEx@users.noreply.github.com>
2018-12-22 11:10:45 -0700

    Update function names with prefix.

--- admin-post-info.php
+++ admin-post-info.php
@@ -10,17 +10,17 @@
  * @package Custom
  */
 
-function rich_edmunds_column( $columns ) {
+function rich_edmunds_api_column( $columns ) {
 	$columns['url']      = esc_html__( 'URL', 'custom' );
 	$columns['template'] = esc_html__( 'Template', 'custom' );
 
 	return $columns;
 }
 
-add_filter( 'manage_page_posts_columns', 'rich_edmunds_column', 10 );
-add_filter( 'manage_post_posts_columns', 'rich_edmunds_column', 10 );
+add_filter( 'manage_page_posts_columns', 'rich_edmunds_api_column', 10 );
+add_filter( 'manage_post_posts_columns', 'rich_edmunds_api_column', 10 );
 
-function rich_edmunds_add_column( $column_name, $post_id ) {
+function rich_edmunds_api_add_column( $column_name, $post_id ) {
 	if ( 'url' === $column_name ) {
 		echo esc_html( wp_make_link_relative( get_permalink( $post_id ) ) );
 	}
@@ -34,5 +34,5 @@ function rich_edmunds_add_column( $column_name, $post_id ) {
 	}
 }
 
-add_filter( 'manage_page_posts_custom_column', 'rich_edmunds_add_column', 10, 2 );
-add_filter( 'manage_post_posts_custom_column', 'rich_edmunds_add_column', 10, 2 );
+add_filter( 'manage_page_posts_custom_column', 'rich_edmunds_api_add_column', 10, 2 );
+add_filter( 'manage_post_posts_custom_column', 'rich_edmunds_api_add_column', 10, 2 );
